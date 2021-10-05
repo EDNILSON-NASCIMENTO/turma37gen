532dd8531b2317347c742982afd3f28b628215c5
EDNILSON-NASCIMENTO <ednilson.nascimento@mail.com>
2021-10-05 10:42:29 -0300

    EXEMPLOS LAÇOS E VETORES

---
+++ java/AULAS/src/lacosDeRepeticao/Aluno.java
@@ -0,0 +1,17 @@
+package lacosDeRepeticao;
+
+public class Aluno {
+	
+	String nome;
+	int nota;
+	String disciplina;
+	
+	
+	@Override
+	public String toString() {
+		return "Aluno [nome=" + nome + ", nota=" + nota + ", disciplina=" + disciplina + "]";
+	}
+	
+	
+	
+}
